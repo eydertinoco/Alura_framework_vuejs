497dbb7f53953fdf0f989a789139187c09c6b6de
Eyder <eydertinoco@outlook.com>
2021-08-25 10:18:29 -0300

    Adicionado componente e encapsulamento

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,80 @@
+<!--
+Para rodar o projeto:
+D:\1. Eyder\2 - SISTEMAS DE INFORMAÇÃO\Alura\VUE.JS\1. Alura_framework_vuejs>  npm run dev
+
+Para rodar a API:
+D:\1. Eyder\2 - SISTEMAS DE INFORMAÇÃO\Alura\VUE.JS\1. Alura_framework_vuejs\api\api>npm start
+> alurapic@1.0.0 start D:\1. Eyder\2 - SISTEMAS DE INFORMAÇÃO\Alura\VUE.JS\1. Alura_framework_vuejs\api\api
+> node server.js
+API escutando na porta: 3000
+http://localhost:3000/v1/fotos
+-->
 <template>
-<div>
-  <h1 v-text="titulo"/>
-  <ul>
-    <li v-for="foto of fotos">
-      <img :src="foto.url" :alt="foto.titulo">
+<div class="corpo">
+  <h1 class="centralizado" v-text="titulo"/>
+
+  <ul class="lista-fotos">
+    <li class="lista-fotos-item" v-for="foto in fotos">
+
+      <meu-painel :titulo="foto.titulo">
+
+        <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+
+      </meu-painel>
+
     </li>
   </ul>
+
 </div>
 </template>
 
 <script>
+import Painel from './components/shared/panel/Painel.vue'
+
 export default {
-  data() {
+
+  components: {
+
+    'meu-painel': Painel
+  },
+
+  data () {
     return {
-      titulo: "alura_animal",
-      fotos: [
-        {
-          url: 'https://www.petz.com.br/blog/wp-content/uploads/2021/03/piercing-para-cachorro-2.jpg',
-          titulo: 'animal_1'
-        },
-        {
-          url: 'https://saude.abril.com.br/wp-content/uploads/2021/03/bichos-foto-vauvau-Getty-Images.png',
-          titulo: 'animal_2'
-        }
-      ]
+      titulo: 'Alurapic', 
+
+      fotos: []
     }
+  },
+  created() {
+
+    this.$http
+      .get('http://localhost:3000/v1/fotos')
+      .then(res => res.json())
+      .then(fotos => this.fotos = fotos, err => console.log(err));
   }
 }
 </script>
 
 <style lang="scss">
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    width: 96%;
+    margin: 0 auto;
+  }
 
+  .centralizado {
+    text-align: center;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos-item {
+    display: inline-block;
+  }
+
+  .imagem-responsiva {
+    width: 100%;
+  }
 </style>
