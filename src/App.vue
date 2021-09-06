d42b9d1cc6ab3fc0af7df165529b87a9ae913cdc
marcos <marcoslucaaaa@gmail.com>
2021-09-06 12:36:41 -0300

    Aplicado SinglePageApplication

--- src/App.vue
+++ src/App.vue
@@ -11,64 +11,35 @@ http://localhost:3000/v1/fotos
 -->
 <template>
 <div class="corpo">
-  <h1 class="centralizado" v-text="titulo"/>
 
-  <input type="search" class="filtro" v-on:input="filtro = $event.target.value" placeholder="filtre por parte do título"/>
+  <nav>
+    <ul>       
+      <li v-for="route in routes">
+        <router-link :to="route.path ? route.path : '/'">
+          {{ route.titulo }}
+        </router-link>
+      </li>
+    </ul>
+  </nav>
 
-  <ul class="lista-fotos">
-    <li class="lista-fotos-item" v-for="foto in fotosComFiltro">
-
-      <meu-painel :titulo="foto.titulo">
-
-        <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
-
-      </meu-painel>
-
-    </li>
-  </ul>
+  <router-view></router-view>
 
 </div>
 </template>
 
 <script>
-import ImagemResponsiva from './components/shared/imagem-responsiva/imagemResponsiva.vue';
-import Painel from './components/shared/panel/Painel.vue'
+
+import { routes } from './routes';
 
 export default {
 
-  components: {
-
-    'meu-painel': Painel,
-    'imagem-responsiva': ImagemResponsiva
-  },
-
-  data () {
+  data() {
     return {
-      titulo: 'Alurapic', 
-      fotos: [],
-      filtro: ''
+      routes: routes
     }
-  },
 
-  computed: {
-    
-    fotosComFiltro() {
-      if(this.filtro) {
-        let exp = new RegExp(this.filtro.trim(), 'i');
-        return this.fotos.filter(foto => exp.test(foto.titulo));
-      } else {
-        return this.fotos;
-      }
-    }
-  },
-
-  created() {
-
-    this.$http
-      .get('http://localhost:3000/v1/fotos')
-      .then(res => res.json())
-      .then(fotos => this.fotos = fotos, err => console.log(err));
   }
+
 }
 </script>
 
@@ -79,20 +50,4 @@ export default {
     margin: 0 auto;
   }
 
-  .centralizado {
-    text-align: center;
-  }
-
-  .lista-fotos {
-    list-style: none;
-  }
-
-  .lista-fotos-item {
-    display: inline-block;
-  }
-
-  .filtro {
-    display: block;
-    width: 100%;
-  }
 </style>
---
+++ src/components/home/Home.vue
@@ -0,0 +1,87 @@
+<template>
+<div>
+  <h1 class="centralizado" v-text="titulo"/>
+
+  <input type="search" class="filtro" v-on:input="filtro = $event.target.value" placeholder="filtre por parte do título"/>
+
+  <ul class="lista-fotos">
+    <li class="lista-fotos-item" v-for="foto in fotosComFiltro">
+
+      <meu-painel :titulo="foto.titulo">
+
+        <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
+
+      </meu-painel>
+
+    </li>
+  </ul>
+
+</div>
+</template>
+
+<script>
+import ImagemResponsiva from '../shared/imagem-responsiva/imagemResponsiva.vue';
+import Painel from '../shared/panel/Painel.vue'
+
+export default {
+
+  components: {
+
+    'meu-painel': Painel,
+    'imagem-responsiva': ImagemResponsiva
+  },
+
+  data () {
+    return {
+      titulo: 'Alurapic', 
+      fotos: [],
+      filtro: ''
+    }
+  },
+
+  computed: {
+    
+    fotosComFiltro() {
+      if(this.filtro) {
+        let exp = new RegExp(this.filtro.trim(), 'i');
+        return this.fotos.filter(foto => exp.test(foto.titulo));
+      } else {
+        return this.fotos;
+      }
+    }
+  },
+
+  created() {
+
+    this.$http
+      .get('http://localhost:3000/v1/fotos')
+      .then(res => res.json())
+      .then(fotos => this.fotos = fotos, err => console.log(err));
+  }
+}
+</script>
+
+<style lang="scss">
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    width: 96%;
+    margin: 0 auto;
+  }
+
+  .centralizado {
+    text-align: center;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos-item {
+    display: inline-block;
+  }
+
+  .filtro {
+    display: block;
+    width: 100%;
+  }
+</style>
