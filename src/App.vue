6a63e0507e168d2412099f41041a4096ce6351a5
marcos <marcoslucaaaa@gmail.com>
2021-09-14 11:07:23 -0300

    Finalizado a aula SinglePageApplication

--- src/App.vue
+++ src/App.vue
@@ -12,17 +12,13 @@ http://localhost:3000/v1/fotos
 <template>
 <div class="corpo">
 
-  <nav>
-    <ul>       
-      <li v-for="route in routes">
-        <router-link :to="route.path ? route.path : '/'">
-          {{ route.titulo }}
-        </router-link>
-      </li>
-    </ul>
-  </nav>
+  <meu-menu :rotas="routes"/>
 
-  <router-view></router-view>
+  <transition name="pagina">
+
+    <router-view></router-view>
+
+  </transition>
 
 </div>
 </template>
@@ -30,8 +26,13 @@ http://localhost:3000/v1/fotos
 <script>
 
 import { routes } from './routes';
+import Menu from './components/shared/menu/Menu.vue'
 
 export default {
+
+  components: {
+    'meu-menu' : Menu
+  },
 
   data() {
     return {
@@ -48,6 +49,14 @@ export default {
     font-family: Helvetica, sans-serif;
     width: 96%;
     margin: 0 auto;
+  }
+
+  .pagina-enter, .pagina-leave-active {
+    opacity: 0;
+  }
+
+  .ppagina-enter-active, .pagina-leave-active {
+    transition: opacity .4s;
   }
 
 </style>
